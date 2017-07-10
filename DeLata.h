e5af3402a8e81a1882fcece370c8a484f395006d
ym <a@b.c>
2017-07-10 23:17:52 +0800

    library is for SAMD-based boards only

--- DeLata.h
+++ DeLata.h
@@ -2,5 +2,11 @@
 #ifndef __DELATA_H__
 #define __DELATA_H__
 
+#include "Arduino.h"
+
+#ifndef ARDUINO_SAMD_VARIANT_COMPLIANCE
+#error this library is not compatible with the selected board
+#endif
+
 
 #endif // __DELATA_H__
